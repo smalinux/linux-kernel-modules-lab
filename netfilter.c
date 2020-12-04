3f3c2adc7e479ab6b300760ed2e1e21bcc05efcd
Sohaib <sohaibfacebook396@gmail.com>
2020-12-04 03:05:13 +0200

    Update

--- netfilter.c
+++ netfilter.c
@@ -7,47 +7,51 @@
 #include <linux/ip.h>
 #include <linux/tcp.h>
 #include <linux/udp.h>
+#include <linux/skbuff.h>
+#include <linux/textsearch.h>
 
 MODULE_AUTHOR("smalinux");
 MODULE_LICENSE("GPL");
 
+
+#define MY_ID "sohaib"
+static	struct ts_config *conf;
+
 static unsigned int *hfunc(void *priv, struct sk_buff *skb, const struct nf_hook_state *state)
 {
-	struct iphdr *iph;
-	struct udphdr *udph;
-	if (!skb)
-		return NF_ACCEPT;
-	pr_err("Yes\n");
-	iph = ip_hdr(skb);
-	if (iph->protocol == IPPROTO_UDP) {
-		udph = udp_hdr(skb);
-		if (ntohs(udph->dest) == 53) {
-			return NF_ACCEPT;
-		}
-	}
-	else if (iph->protocol == IPPROTO_TCP) {
-		return NF_ACCEPT;
-	}
-	
-	return NF_DROP;
+	int pos;
+	struct ts_state st;
+
+	memset(&st, 0, sizeof(struct ts_state));
+
+	for (pos = skb_find_text(skb, 0, INT_MAX, conf);
+		pos != UINT_MAX;
+		pos = textsearch_next(conf, &st))
+		pr_err("pos at %d\n", pos);
+
+	return NF_ACCEPT;
 }
 
 
 /* Initialize netfilter hook */
 static struct nf_hook_ops nfho = {
 	.hook 		= (nf_hookfn*)hfunc,
-	.hooknum 	= NF_INET_POST_ROUTING,		/* received packets */
-	.pf 		= PF_INET,			/* IPv4 */
+	.hooknum 	= NF_INET_POST_ROUTING,
+	.pf 		= PF_INET,
 	.priority 	= NF_IP_PRI_FIRST,		/* max hook priority */
 };
 
 static int __init LKM_init(void)
 {
+	conf = textsearch_prepare("kmp", MY_ID, 6, GFP_KERNEL, TS_AUTOLOAD);
+	if (IS_ERR(conf))
+		return PTR_ERR(conf);
 	nf_register_net_hook(&init_net, &nfho);
 }
 
 static void __exit LKM_exit(void)
 {
+	textsearch_destroy(conf);
 	nf_unregister_net_hook(&init_net, &nfho);
 }
 
